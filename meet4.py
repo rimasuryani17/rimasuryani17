deec7a62f6afa2642678e1c088904566910c7f0c
rimasuryani17 <rimasuryani1724@gmail.com>
2024-07-01 17:22:16 -0700

    Add files via upload

---
+++ meet4.py
@@ -0,0 +1,44 @@
+#aritmatika
+
+x = 12
+y = 5
+
+#penjumlahan
+
+hasil = 12 + 5
+print(x ,"+" ,y ,"=",hasil)
+
+#pengurangan
+hasil = 12 - 5
+print(x ,"-" ,y ,"=",hasil)
+
+#perkalian
+hasil = 12 * 5
+print(x ,"x" ,y ,"=",hasil)
+
+#pembagian
+hasil = 12 / 5
+print(x ,"/" ,y ,"=",hasil)
+
+#perpangkatan
+hasil = 12 ** 5
+print(x ,"^" ,y ,"=",hasil)
+
+#modulus
+hasil = 12 % 5
+print(x ,"mod" ,y ,"=",hasil)
+
+#floor devision
+hasil = 12 // 5
+print(x ,"floor" ,y ,"=",hasil)
+
+#skala prioritas
+x = 12
+y = 3
+z = 5
+hasil =(x ** y * (z + x) / y - z % x // y)
+print( x, "**",y,"*",z,"+", x, "/" ,y ,"-", z ,"%" ,x,"//", y)
+print(hasil)
+
+
+

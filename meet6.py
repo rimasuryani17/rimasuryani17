f0e05857ac8664e7a9ba85d7815f3d517978d91b
rimasuryani17 <rimasuryani1724@gmail.com>
2024-07-05 09:25:04 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,25 @@
+
+
+x = 3
+y = 4
+
+#lebih besar
+hasil = x > y
+print (x, '>', y, '=', hasil)
+
+hasil = x < y
+print (x, '<', y, '=', hasil)
+
+#hasil besar >=
+hasil  = x >= y
+print (x, '>=',4, '=', hasil)
+print (x, '>=',3, '=', hasil)
+
+
+hasil = x <= y
+print (x, '<=',4, '=', hasil)
+print (x, '<=',3, '=', hasil)
+
+hasil = x <= y
+print (x, '<=',4, '=', hasil)
+print (x, '<=',3, '=', hasil)
